3c1812d02ef308862b2e631687a70026eddd8356
Victor-Watanabe <watavh@hotmail.com>
2024-07-01 22:02:55 -0300

    equation_interest

--- Interest_equation.py
+++ Interest_equation.py
@@ -2,4 +2,11 @@ import PySimpleGUI as sg
 import formula
 
 def equation_interest(number_float,percentage_float,month):
-    print(number_float,percentage_float,month)
+    montante = number_float * (1 + percentage_float / 100) ** month
+    montante = round(montante,2)
+    total_invested = number_float
+    fees = montante - number_float
+    fees = round(fees,2)
+    number_float = round(number_float,2)
+
+    print(f"O resultado de investimento após {month} meses: \n O total de Juros foi de {fees}; \n O total arrecadado foi de {montante};\n Com o valor de Capital Inicial de {number_float}.")
--- interest_equation_with_addition.py
+++ interest_equation_with_addition.py
@@ -2,4 +2,7 @@ import PySimpleGUI as sg
 import formula
 
 def equation_interest_addition(number_float,percentage_float,month):
-    print(number_float,percentage_float,month)
+    
+    montante = number_float*1+percentage_float**month 
+
+    print(montante)
